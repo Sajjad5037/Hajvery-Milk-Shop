bf46c3d7eb47cac8c2c6d1e83743c652b814b696
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-09-10 02:42:04 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import OrderOnline from "./pages/ShopOnline";
+import ShopOnline from "./pages/ShopOnline";
 
 function App() {
   return (
