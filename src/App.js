cd223623ee927ca5d6150372f7fb494cf86310d4
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-09-10 02:29:34 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
-import ProductDetails from "./pages/ProductDetails";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+
+
+
 import OrderOnline from "./pages/OrderOnline";
 
 function App() {
